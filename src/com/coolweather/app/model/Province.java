083ba8d621f2583702ad4e754018405a6f22de2d
Tony <tony@gmail.com>
2016-04-19 14:48:03 +0800

    加入显示天气信息的功能。

---
+++ src/com/coolweather/app/model/Province.java
@@ -0,0 +1,32 @@
+package com.coolweather.app.model;
+
+public class Province {
+	private int id;
+	private String provinceName;
+	private String provincecode;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProvinceName() {
+		return provinceName;
+	}
+
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+
+	public String getProvincecode() {
+		return provincecode;
+	}
+
+	public void setProvincecode(String provincecode) {
+		this.provincecode = provincecode;
+	}
+
+}
